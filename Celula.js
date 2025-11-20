cd398b4fd741f0bb7f8c0ec9398c900c1453e7b5
marinopepinillo <m.perona.2024@alumnos.urjc.es>
2025-11-20 13:02:22 +0100

    terminar clase celula

--- Celula.js
+++ Celula.js
@@ -12,5 +12,22 @@ class Celula{
         }
         this.estado = estado;
     }
-    
+
+    calcularSiguienteEstado(vecinasVivas){ //aplicamos la logica de la supervivencia de las reglas
+        if(!this.estado && vecinasVivas === 3){
+            this.nextEstado = true; //si esta muerta y tiene 3 vecinas vivas nace
+        }else if(this.estado && (vecinasVivas === 2 || vecinasVivas === 3)){
+            this.nextEstado = true; // si esta viva y tiene 2 o 3 vecinas vivas sobrevive
+        }   else{
+            this.nextEstado = false; // muere por soledad o sobrepoblacion
+        }
+
+    }
+
+    actualizarEstado(){
+        this.setEstado(this.nextEstado);
+        if(this.estado){
+            this.time += 1; // si esta viva aumenta el tiempo que ha estado viva
+        }
+    }
 }
