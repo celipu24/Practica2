d0accf20831a23c4e392915555c1fb38f219a7be
marinopepinillo <m.perona.2024@alumnos.urjc.es>
2025-11-20 12:37:02 +0100

    creacionclasecelula1

--- Celula.js
+++ Celula.js
@@ -1,7 +1,16 @@
-class Celula(){
-    constructor(time, estado){
-        this.estado = estado;
-        this.time = time;
+class Celula{
+    constructor(){
+        this.estado = false; // false = muerta, true = viva
+        this.time = 0; // tiempo que ha estado viva
+        this.nextEstado = false; // estado para la siguiente generación
 
     }
+
+    setEstado(estado){
+        if(this.estado !== estado){
+            this.time = 0; // reiniciar el tiempo si cambia de estado
+        }
+        this.estado = estado;
+    }
+    
 }
