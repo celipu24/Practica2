e241f46a59d921f81b1fd86dc1180b3b8ebee085
celipu24 <celipupe2006@gmail.com>
2025-11-20 12:43:40 +0100

    creacion de matriz de tablero

--- Mundo.js
+++ Mundo.js
@@ -1,6 +1,20 @@
-class Mundo(){
-    constructor(ancho){
-        this.ancho = ancho;
-        this.matriz = new Array(ancho * ancho);
+class Mundo{
+    constructor(ancho=40){
+        this.ancho = Number(ancho) || 40;
+        this.matriz = [];
+        this.crearMatriz();
+    }
+
+    /*Este método crea una matriz de tamaño ancho x ancho inicializada con null
+    y se van rellenando las casillas con un bucle que recorre las filas y columnas */
+    crearMatriz(){
+        this.matriz = new Array(this.ancho);
+        for(let fila=0; fila < this.ancho; fila++){
+            this.matriz[fila] = new Array(this.ancho);
+            for(let columna=0; columna < this.ancho; columna++){
+                this.matriz[i][j] = null;
+            }
+        }
     }
 }
+
